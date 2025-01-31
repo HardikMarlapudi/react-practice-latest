f9f4d48c9db0b556c02cda8e7192ce07915cdff3
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-01-31 00:06:01 -0500

    adding new version of react-practice

---
+++ react-project-latest/src/Objective.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './main.jsx'
+import './Objective.css'
+
+function Objective() {
+  return (
+    <div>
+
+      <div id="objective">
+      <h1>Objective</h1>
+      <p>-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+      </div>
+
+      <div id="about-objective">
+      <p>A dedicated and aspiring data professional with a solid foundation in Computer Information Systems, complemented by extensive coursework in data analysis, database management, and programming. Possesses a keen aptitude for transforming raw data into actionable insights through expertise in data-driven methodologies, statistical modeling, and visualization tools. Demonstrates strong problem-solving skills, combined with an analytical mindset, to tackle complex challenges and deliver impactful solutions.
+
+Proficient in key technologies such as Python, SQL, R, and data visualization platforms like Tableau and Power BI, with hands-on experience in building scalable databases, performing ETL processes, and conducting predictive analysis. Skilled in leveraging machine learning algorithms and statistical techniques to uncover patterns and drive decision-making. Enthusiastic about applying analytical skills and technical expertise to contribute to data-centric projects in dynamic, fast-paced, and innovative technological environments.
+
+Committed to continuous learning and professional development, with a passion for staying at the forefront of advancements in data science, artificial intelligence, and big data technologies. Thrives in collaborative settings, bringing a results-oriented approach and a drive to deliver measurable outcomes that align with organizational goals. Aims to bridge the gap between technical data capabilities and business needs, empowering teams with data-backed strategies and solutions.
+
+</p>
+      </div>
+    </div>
+  );
+}    
+
+export default Objective;
---
+++ react-project-latest/src/TechnicalExperience.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './main.jsx'
+import './Teaching&Technical.css'
+
+function TechnicalExperience() {
+    
+    return (
+        <>
+
+        <div id = "teaching-technical">
+            <h2>Teaching & Technical</h2>
+        </div>
+
+        <ul>
+            <li>UofSC Cybersecurity Club</li>
+            <li>Teaching Assistant: CSCE 146</li>
+            
+        </ul>
+        </>
+    )
+
+
+}
+
+export default TechnicalExperience;
