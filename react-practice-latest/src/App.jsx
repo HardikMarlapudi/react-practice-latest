79d4fbf424a3e9b9777ef66efe2471d033ad1486
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-04-13 01:56:17 -0400

    fixed line headers and styled the resume webpage

--- react-practice-latest/src/App.jsx
+++ react-practice-latest/src/App.jsx
@@ -1,14 +1,11 @@
-
 import React from 'react';
 import './App.css';
-import './main.jsx'
+import './main.jsx';
 
 export default function App() {
   return (
-    
     <div className="title">
       <h1>Hardik Marlapudi</h1>
-      
     </div>
   )
 }
